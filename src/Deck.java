05d60bd8749050ef15209b9512f2675b59b0ce2b
erikmalm <70799618+erikmalm@users.noreply.github.com>
2020-12-02 14:49:40 +0100

    edited the counting mechanisms

--- src/Deck.java
+++ src/Deck.java
@@ -1,5 +1,6 @@
 import java.util.NoSuchElementException;
 import java.util.Random;
+import static java.lang.System.*;
 
 public class Deck {
 
@@ -8,9 +9,6 @@ public class Deck {
     // These are all the cards that have been dealt from the deck
     Card [] cards;
 
-    // We want to keep track of the amount of cards left in the deck
-    private int cardsInDeck = 52;
-
     // There are four typs of suites
     // [0] = "hearts"
     // [1] = "clubs"
@@ -18,11 +16,31 @@ public class Deck {
     // [3] = "spades"
     String [] suites = {"hearts","clubs","diamonds","spades"};
 
-    // We also want to keep track of the different suites
-    private int heartsInDeck = 13;
-    private int clubsInDeck = 13;
-    private int diamondsInDeck = 13;
-    private int spadesInDeck = 13;
+    // We also want to keep track of the remaining card in the suites
+    // When a card is removed, we will remove the integer by changing it to -1
+    // We will use these arrays when counting all the cards as well
+    private int [] hearts = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+    private int [] clubs = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+    private int [] diamonds = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+    private int [] spades = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+
+    // The deck can be seen as a matrix of 4 suites and 13 values of each suite
+    private int [][] deck = new int [4][13];
+
+    // There are four typs of suites and they are placed in the following positions:
+    // [0] = "hearts"
+    // [1] = "clubs"
+    // [2] = "diamonds"
+    // [3] = "spades"
+
+    // We need to generate the deck:
+/*
+    for (int i = 0; i < 4 ; i ++ )
+        for (int j = 0; i < 13; j++)
+            deck[i][j] = j + 1;
+            
+ */
+
 
 
     /**************************************************
@@ -32,28 +50,54 @@ public class Deck {
 
     // This method allows the user to draw a card from the deck
     // This will generate a new card
-    // TODO Create an exception!
     public void drawCard()
+    throws NoMoreCardsException
     {
-        Card []     cards = new Card[53 - cardsInDeck];
+        if (leftInDeck() == 0)
+            throw new NoMoreCardsException ("Can't draw more cards, deck is empty!");
+
+        Card []     cards = new Card[53 - leftInDeck()];
         int i = 0;
 
-        if (cardsInDeck < 52)
+        if (leftInDeck() < 52)
             for (i = 0; i < cards.length - 1; i ++)
                 cards[i] = this.cards[i];
 
         cards[i] = randomCard();
         this.cards = cards;
+        removeCardFromDeck(cards[i]);
+    }
 
-        cardsInDeck --;
-        if (cards[i].getSuite().equals("hearts"))
-            heartsInDeck --;
-        if (cards[i].getSuite().equals("clubs"))
-            clubsInDeck --;
-        if (cards[i].getSuite().equals("diamonds"))
-            diamondsInDeck --;
-        if (cards[i].getSuite().equals("spades"))
-            spadesInDeck --;
+    // The method deal card utilizes the draw card method, but also
+    // returns a copy of the new card
+
+    public Card dealCard() {
+
+        int position = 52 - leftInDeck();
+        drawCard();
+        return getCard(position);
+    }
+
+    // The method copies the card in the current position
+    private Card getCard (int position)
+    {
+        Card card = new Card(cards[position]);
+        return card;
+    }
+
+    private void removeCardFromDeck(Card card) {
+
+        if (card.getSuite().equals("hearts"))
+            hearts[card.getValue() - 1] = -1;
+
+        if (card.getSuite().equals("clubs"))
+            clubs[card.getValue() - 1] = - 1;
+
+        if (card.getSuite().equals("diamonds"))
+            diamonds[card.getValue() - 1] = - 1;
+
+        if (card.getSuite().equals("spades"))
+            spades[card.getValue() - 1] = - 1;
     }
 
     // Method that creates a new random card from the deck,
@@ -65,7 +109,8 @@ public class Deck {
         int suite = rand.nextInt(availableSuites.length);
 
         // Now we can obtain its value
-        int [] availableValues = availableValues(availableSuites[suite]);
+        // int [] availableValues = availableValues(availableSuites[suite]);
+        int [] availableValues = getRemainingCardsOfSuite(availableSuites[suite]);
         int value = rand.nextInt(availableValues.length);
 
         // Knowing its suite and value, we can now create the card
@@ -77,33 +122,6 @@ public class Deck {
      * AND VALUES OF CARDS
      * ***********************************************/
 
-    private int[] availableValues(String suite) {
-
-        // By default, the available cards are 1 to 13 as follows
-        int [] availableCards = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-
-        // We will begin by counting the number of cards left in our suite
-        int countValuesOfSuite = countValuesOfSuite(suite);
-
-        // This will remove all cards from array availableCards by changing their value to -1
-        // Their position is by default relative to their value by - 1
-        if (cardsInDeck < 52)
-            for (Card card : cards)
-                if (card.getSuite().equals(suite))
-                    availableCards[card.getValue() - 1] = -1;
-
-        // We can now declare our array of available values, which should hold
-        // all the available cards of our current suite
-        int [] availableValues = new int [countValuesOfSuite];
-        int j = 0;
-
-        // This assigns the available values to an array of available values
-        for (int availableCard : availableCards)
-            if (availableCard > 0)
-                availableValues[j++] = availableCard;
-
-        return availableValues;
-    }
 
     // The method returns an array of the available suites for the user
     private String [] availableSuites()
@@ -131,27 +149,102 @@ public class Deck {
 
     // Method to count the amount of cards left in each suite
     // This method can also easily be used to check if the suit has any cards left
-    private int countValuesOfSuite(String suite) {
+    public int countValuesOfSuite(String suite) {
+
+        int amount = 0;
 
         if (suite.equals("hearts"))
-            return heartsInDeck;
+            for (int heart : hearts)
+                if (heart > 0)
+                    amount++;
 
         if (suite.equals("clubs"))
-            return clubsInDeck;
+            for (int club : clubs)
+                if (club > 0)
+                    amount++;
 
         if (suite.equals("diamonds"))
-            return diamondsInDeck;
+            for (int diamond : diamonds)
+                if (diamond > 0)
+                    amount++;
 
         if (suite.equals("spades"))
-            return spadesInDeck;
+            for (int spade : spades)
+                if (spade > 0)
+                    amount++;
 
-        else {
-            return 13;
+        return amount;
+    }
+
+    // The method counts the amount of cards remaining in the deck
+    public int leftInDeck ()
+    {
+        int amount = 0;
+
+        for (int i = 0; i < 4; i ++)
+            amount += getRemainingCardsOfSuite(suites[i]).length;
+
+        return amount;
+    }
+
+    public int [] getCardsOfSuite(String suite) {
+
+        int j = 0;
+
+        if (suite.equals("hearts")) {
+            int [] copyHearts = new int [hearts.length];
+            arraycopy(hearts, 0, copyHearts, 0, hearts.length);
+            return copyHearts;
+        }
+
+        if (suite.equals("clubs")) {
+            int [] copyClubs = new int [clubs.length];
+            arraycopy(clubs, 0, copyClubs, 0, clubs.length);
+            return copyClubs;
+        }
+
+        if (suite.equals("diamonds")) {
+            int [] copyDiamonds = new int [diamonds.length];
+            arraycopy(diamonds, 0, copyDiamonds, 0, diamonds.length);
+            return copyDiamonds;
+        }
+
+        if (suite.equals("spades")) {
+            int [] copySpades = new int [spades.length];
+            arraycopy(spades, 0, copySpades, 0, spades.length);
+            return copySpades;
+        }
+
+        return null;
+    }
+
+    public int [] getRemainingCardsOfSuite (String suite) {
+
+        int[] copy = getCardsOfSuite(suite);
+        int[] remainingCards = new int[countValuesOfSuite(suite)];
+        int j = 0;
+
+        for (int i = 0; i < copy.length; i++)
+            if (copy[i] > 0)
+                remainingCards[j++] = copy[i];
+
+        return remainingCards;
+    }
+
+    // EXCEPTIONS
+
+    public class NoMoreCardsException extends NoSuchElementException
+    {
+        public NoMoreCardsException ()
+        {
+            super ();
+        }
+
+        public NoMoreCardsException (String message)
+        {
+            super (message);
         }
     }
 
-    public int leftInDeck ()
-    {
-        return cardsInDeck;
-    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,116 @@ public class Main {
     public static void main (String [] args)
     {
         Deck deck = new Deck();
+        int cardCount = 0;
+
+
 
 
         for (int i = 0; i < 52; i ++)
             deck.drawCard();
+/*
+
+        printCardsLeftOfSuite(deck, "hearts");
+        printCardsLeftOfSuite(deck, "clubs");
+        printCardsLeftOfSuite(deck, "diamonds");
+        printCardsLeftOfSuite(deck, "spades");
+
+ */
+
 
 
         for (int i = 0; i < deck.cards.length; i ++)
             System.out.println(i + ": " + deck.cards[i]);
 
         System.out.println("Cards left in deck: " + deck.leftInDeck());
+
+
+
+
+
+        // Deal new hand
+
+        /*
+
+        Card [] playerHand = new Card[5];
+
+        for (int i = 0; i < playerHand.length; i ++) {
+            deck.drawCard();
+            playerHand[i] = deck.cards[cardCount++];
+        }
+
+        for (int i = 0; i < playerHand.length; i ++)
+            System.out.println(i + ": " + playerHand[i]);
+
+        highestRank(playerHand);
+
+         */
+
+
+
+
+
     }
+
+    private static void printCardsLeftOfSuite(Deck deck, String suite) {
+
+        System.out.print(suite + " left: ");
+
+        System.out.print(deck.countValuesOfSuite(suite));
+
+        System.out.print(" with the following values: ");
+
+        int [] cardsOfSuite = deck.getRemainingCardsOfSuite(suite);
+
+
+        for (int i = 0; i < cardsOfSuite.length - 1; i ++)
+            System.out.print(cardsOfSuite[i] + ", ");
+
+        System.out.print(cardsOfSuite[cardsOfSuite.length -1]);
+
+        System.out.println("");
+    }
+
+    private static void highestRank(Card[] hand) {
+
+        Card [] sortHand = new Card [hand.length];
+
+        int x = 0;
+
+        for (Card card : hand)
+            sortHand[x ++] = new Card (card);
+
+        for (int i = 0; i < sortHand.length; i ++) {
+            boolean changed = false;
+            int index = 1;
+            for (int j = 1 + i; j < sortHand.length; j++) {
+                if (sortHand[j].getValue() < sortHand[index].getValue()) {
+                    index = j;
+                    changed = true;
+                }
+            }
+
+            Card leastValueCard = new Card(sortHand[index]);
+            Card higherValueCard = new Card(sortHand[i]);
+        }
+
+
+
+        // int [] same = new int [hand.length];
+
+        /*
+
+        for (int i = 0; i < hand.length; i++)
+            for (int j = 0; j < hand.length - 1; j++)
+                if (hand[i].getValue() == hand[j + 1].getValue())
+                    same[i] ++;
+
+        for (int i = 0; i < same.length; i ++)
+            System.out.println(same[i]);
+
+         */
+
+    }
+
+
 }
