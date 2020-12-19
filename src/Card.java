69ee65f1eb53d584bb8e482a2248cba58200285b
erikmalm <70799618+erikmalm@users.noreply.github.com>
2020-12-19 18:35:59 +0100

    updated naming convention

--- src/Card.java
+++ src/Card.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
+
 // ♦♣♥♠
+
 
 public class Card {
 
-    private String suite;
-    private int position;
+    private String      suite;
+    private int         position;
 
     // Constructor for a new card using String (suite) and Int (value)
     public Card (String suite, int position)
@@ -26,6 +28,7 @@ public class Card {
     }
 
     // The value is equal to it's position in the array + 1
+    // unless it's an ACE, then we have to add 13!
     public int getValue() {
         int value = position + 1;
 
@@ -35,32 +38,55 @@ public class Card {
         return value;
     }
 
+    // This method simply returns the position of the card
     public int getPosition()
     {
         return position;
     }
 
+    // This is what should be returned when printing card (text)
     public String toString()
     {
         StringBuilder sb = new StringBuilder("[");
 
         if (this.position == 1)
-            sb.append("ace of " + this.suite);
+            sb.append("A " + getSuiteSymbol());
 
-        else if (position > 1 && position < 11)
-            sb.append(position + " of " + this.suite);
+        if (position > 1 && position < 11)
+            sb.append(position + " " + getSuiteSymbol());
 
-        else if (this.position == 11)
-            sb.append("jack of " + this.suite);
+        if (this.position == 11)
+            sb.append("J " + getSuiteSymbol());
 
-        else if (this.position == 12)
-            sb.append("queen of " + this.suite);
+        if (this.position == 12)
+            sb.append("Q " + getSuiteSymbol());
 
-        else if (this.position == 13)
-            sb.append("king of " + this.suite);
+        if (this.position == 13)
+            sb.append("K " + getSuiteSymbol());
 
         sb.append("]");
 
         return sb.toString();
+    }
+
+    // ♦♣♥♠
+
+    public String getSuiteSymbol() {
+        StringBuilder sb = new StringBuilder("");
+
+        if (this.suite.equals("hearts"))
+            sb.append("♥");
+
+        if (this.suite.equals("spades"))
+            sb.append("♠");
+
+        if (this.suite.equals("diamonds"))
+            sb.append("♦");
+
+        if (this.suite.equals("clubs"))
+            sb.append("♣");
+
+        return sb.toString();
+
     }
 }
